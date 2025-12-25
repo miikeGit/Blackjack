93fa63b35e75bce0b3b464e9c44f3b65d59ac034
Mykhailo <mmmalets10@gmail.com>
2025-12-25 02:10:49 +0200

    migrate project to QT Creator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
-#include "Deck.h"
+#include "mainwindow.h"
 
-int main() {
-  return 0;
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
